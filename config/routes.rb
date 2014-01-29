76a42d26ca00b863cc488085ca946087c21fed3b
QiangDeng <ericdeng11@gmail.com>
2014-01-29 00:09:59 -0500

    CHANGE match

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,7 @@ Wlglobal::Application.routes.draw do
 
     root :to => 'welcome#index'
     
-    
-    unless Rails.application.config.consider_all_requests_local
-		match '*not_found', to: 'errors#error_404'
-	end
+
 
     get "welcome/index"
     
