6b3203c9b910336964c225aa96bbb7ab769392bc
QiangDeng <ericdeng11@gmail.com>
2014-03-07 09:19:01 -0500

    Changes

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,19 +2,16 @@ class Customer
   include Mongoid::Document
     field :address, :type => String, :default => ""
     field :customer_id, :type => String, :default => ""
+    
     field :us_phone, :type => String, :default => ""
     field :cn_phone, :type => String, :default => ""
-    field :us_phone1, :type => String, :default => ""
-    field :cn_phone1, :type => String, :default => ""
-    field :us_phone2, :type => String, :default => ""
-    field :cn_phone2, :type => String, :default => ""
-    field :us_phone3, :type => String, :default => ""
-    field :cn_phone3, :type => String, :default => ""
+    
     field :company, :type => String, :default => ""
     field :wechat, :type => String, :default => ""
     field :name, :type => String, :default => ""
     field :sex, :type => String, :default => ""
     field :status, :type => String, :default => ""
+    field :date_of_birth, :type => String, :default => ""
     field :project, :type => String, :default => ""
     
     #Other people in the family
@@ -24,6 +21,15 @@ class Customer
     field :wechat1, :type => String, :default => ""
     field :wechat2, :type => String, :default => ""
     field :wechat3, :type => String, :default => ""
+    field :us_phone1, :type => String, :default => ""
+    field :cn_phone1, :type => String, :default => ""
+    field :us_phone2, :type => String, :default => ""
+    field :cn_phone2, :type => String, :default => ""
+    field :us_phone3, :type => String, :default => ""
+    field :cn_phone3, :type => String, :default => ""
+    field :date_of_birth1, :type => String, :default => ""
+    field :date_of_birth2, :type => String, :default => ""
+    field :date_of_birth3, :type => String, :default => ""
     
     #User activities 
     field :firstAct, :type => String, :default => ""
